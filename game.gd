b530379f2c3a76cb9bef5279d9e0985987d1cfec
G-Shadow <18046412+gshadows@users.noreply.github.com>
2023-08-06 10:52:24 +0300

    Add plan and exports preset. Fix settings load.

--- game.gd
+++ game.gd
@@ -5,13 +5,25 @@ signal started
 
 var active := false
 
+@onready var AUDIO_BUS_MASTER = AudioServer.get_bus_index("Master")
+@onready var AUDIO_BUS_MUSIC = AudioServer.get_bus_index("Music")
+@onready var AUDIO_BUS_SFX = AudioServer.get_bus_index("SFX")
+
 
 func _ready():
 	Settings.audio_setup_updated.connect(_on_audio_setup_upd)
+	Settings.video_setup_updated.connect(_on_video_setup_upd)
 
 
 func _on_audio_setup_upd():
-	pass
+	AudioServer.set_bus_volume_db(AUDIO_BUS_MASTER, linear_to_db(Settings.audio.master_vol))
+	AudioServer.set_bus_mute(AUDIO_BUS_MASTER, !Settings.audio.master_enabled)
+
+	AudioServer.set_bus_volume_db(AUDIO_BUS_SFX, linear_to_db(Settings.audio.sfx_vol))
+	AudioServer.set_bus_mute(AUDIO_BUS_SFX, !Settings.audio.sfx_enabled)
+
+	AudioServer.set_bus_volume_db(AUDIO_BUS_MUSIC, linear_to_db(Settings.audio.music_vol))
+	AudioServer.set_bus_mute(AUDIO_BUS_MUSIC, !Settings.audio.music_enabled)
 
 func _on_video_setup_upd():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if Settings.video.full_screen else DisplayServer.WINDOW_MODE_MAXIMIZED)
--- utils/simple_json.gd
+++ utils/simple_json.gd
@@ -15,9 +15,11 @@ static func save_to_file(obj, file_name:String):
 
 static func load_from_string(obj, json:String):
 	var test_json_conv = JSON.new()
-	test_json_conv.parse(json)
-	var result = test_json_conv.get_data()
-	var dict = result.result
+	if not test_json_conv.parse(json):
+		printerr("Invalid JSON format")
+		return
+	#var result = test_json_conv.get_data()
+	var dict = test_json_conv.get_data() #result.result
 	if not dict is Dictionary:
 		printerr("Invalid JSON format: dictionary expected")
 		return
